ba7befb8f07aa03c044d66ebfb55f02a0f5e49da
Xiaohui <286390860@qq.com>
2016-11-06 22:49:51 +0800

    publish for v0.1.0

--- mix.exs
+++ mix.exs
@@ -2,19 +2,29 @@ defmodule ExWechat.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_wechat,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ex_wechat,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+     ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :floki]]
+  end
+
+  defp description do
+    """
+    A Phoenix/Elixir Wechat Api.
+    """
   end
 
   # Dependencies can be Hex packages:
@@ -28,10 +38,20 @@ defmodule ExWechat.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:ex_doc, github: "elixir-lang/ex_doc", only: :dev},
       {:httpoison, "~> 0.9.0"},
       {:poison, "~> 2.0"},
       {:plug, "~> 1.2.2"},
       {:floki, "~> 0.11.0"}
+    ]
+  end
+
+  defp package do
+    [
+      name: :ex_wechat,
+      maintainers: ["h1u2i3"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/h1u2i3/ex_wechat"}
     ]
   end
 end
